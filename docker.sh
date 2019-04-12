a284628a4f900a780e865d0c197e4a47522a8b0f
Brian Ail <ail.brianez@gmail.com>
2019-04-12 18:29:01 -0300

    Docker andando?

--- docker.sh
+++ docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-docker build . --tag ${whoami}/so_builder
 
-docker run -v ${PWD}:/sources/ ${whoami}/so_builder
+docker build . --tag $(whoami)/so_builder
+
+docker run -v ${PWD}:/sources/ $(whoami)/so_builder
+
+sudo chown $(whoami):$(whoami) Image/x*
+sudo chmod 777 Image/x*
 
 ./run.sh
