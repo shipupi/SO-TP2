227feaeb97baa01fc605939cf4d86cfde5a934a2
Brian Ail <ail.brianez@gmail.com>
2019-04-28 17:53:47 -0300

    fix inicializacion de punteros de MM

--- Kernel/memoryManager.c
+++ Kernel/memoryManager.c
@@ -5,24 +5,23 @@
 
 void * getMemorySpace( int32_t pid, uint64_t requestedSpace);
 void initializeMemoryManager();
-void freeMemorySpace (int32_t pid, void * freeBaseAddress);
+void freeMemorySpace (void * freeBaseAddress,int32_t size);
 
 #define MAXBLOCKS 32768
 #define BLOCKSIZE 4096
-static byte blockStatus[MAXBLOCKS];
-static void * baseAddress = 0x900000;
-static uint64_t size = 0x8000000;//128Mb = 2ˆ7*2ˆ10 = 131072
+static char blockStatus[MAXBLOCKS];
+static void * baseAddress = (void *)(uintptr_t) 0x900000;
 
 void * getBlockById(int32_t N){
-	return baseAddress + N * blocksSize ;
+	return baseAddress + N * BLOCKSIZE ;
 }
 
 int32_t getIdByAdress(void * address){
-	return (address - baseAddress) / blocksSize ;
+	return (address - baseAddress) / BLOCKSIZE ;
 }
 
 int32_t getBlocksForSize(uint64_t requestedSize){
-	return (requestedSize / blocksSize)+1;
+	return (requestedSize / BLOCKSIZE)+1;
 }
 
 void initializeMemoryManager(){
@@ -33,7 +32,7 @@ void initializeMemoryManager(){
 
 void * requestMemorySpace(uint64_t requestedSpace){
 	int32_t n = getBlocksForSize(requestedSpace); //me must find n contiguous 0's in an array
-	void * ret = -1;
+	void * ret = (char *)(uintptr_t) -1;
 	int32_t base = 0;
 
 	int acum = 0;
