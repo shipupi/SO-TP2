f29458b93051900b717cc735c538a6505f5be9ea
Brian Ail <ail.brianez@gmail.com>
2019-05-30 16:15:13 -0300

    Fixed algunos includes

--- Kernel/screenManager.c
+++ Kernel/screenManager.c
@@ -1,11 +1,13 @@
 #include <stdint.h>
-#include "drivers/vesaDriver.h"
-#include "scheduler/PCB.h"
-#include "ipc/ipc.h"
-#include "screenManager.h"
+#include "include/drivers/vesaDriver.h"
+#include "include/scheduler/PCB.h"
+#include "include/ipc/ipc.h"
+#include "include/screenManager.h"
 
-
+// LA 'b' esta puesta como placeholder para que reserve el lugar en el data segment y no en el bss
 char buf[MAXFDSIZE] = {'b',0};
+
+
 void screenManager() {
 
 	ipc_create(DEFAULT_FDOUT, MAXFDSIZE);
