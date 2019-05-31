0141239b58dfa20ab3d23ad8e8979142c6c5e550
Lucio Pagni <luciopagni@Lucios-MacBook-Air.local>
2019-05-31 20:09:09 -0300

    diph

--- Userland/SampleCodeModule/diph.c
+++ Userland/SampleCodeModule/diph.c
@@ -4,11 +4,111 @@
 #include "stdio.h"
 #include "syscalls.h"
 #include "applications.h"
-#define n 4
- 
-void diph(){
-    printf("DIPH\n");
+#include "include/PCB.h"
+#define N 4
+
+uint8_t philosophers[N];
+int philos;
+
+int id = 5;
+
+int random(){
+	return 5;
 }
+
+void think(int time){
+	while(10000*time){
+
+	}
+}
+
+void eat(){
+	int r = random();
+	while(10000*r){
+
+	}
+}
+
+void philosopher(){
+	int i = 0;
+	//THINKING
+	while(1){
+		think(random());
+		char * s;
+		if(id%2==0){
+
+			//HUNGRY
+
+    		numberToString(id,s);
+			os_mut_request(s);
+			numberToString(id+1,s);
+			os_mut_request(s);
+			
+			//EATING
+
+			eat();
+
+			numberToString(id,s);
+			os_mut_release(s);
+			numberToString(id+1,s);
+			os_mut_release(s);
+
+		}
+		else{
+
+			//HUNGRY
+
+			numberToString(id+1,s);
+			os_mut_request(s);
+			numberToString(id,s);
+			os_mut_request(s);
+			
+			//EATING
+
+			eat();
+
+			numberToString(id+1,s);
+			os_mut_release(s);
+			numberToString(id,s);
+			os_mut_release(s);
+
+		}
+		think(random());
+	}
+	//DONE
+}
+
+void diph(){
+	
+	printf("DIPH\n");
+
+	philos = N ;
+
+    int i;
+
+    for(i=0;i<philos;i++){
+    	char * s;
+    	numberToString(i,s);
+    	os_mut_create(s);
+    }
+
+    for(i=0;i<philos;i++){
+    	philosophers[i] = os_addProcess(&philosopher,1,'c',PCB_BACKGROUND,4000);
+    }
+
+    //IMPRIMIR
+
+    //CAMBIAR LA CANTIDAD DE FILOSOFOS EN RUNTIME
+    
+    for(i=0;i<philos;i++){
+    	char * s;
+    	numberToString(i,s);
+    	os_mut_delete(s);
+    }
+}
+
+
+
 
 
 
--- Userland/SampleCodeModule/include/string.h
+++ Userland/SampleCodeModule/include/string.h
@@ -1,9 +1,9 @@
 #ifndef STRING_H
 #define STRING_H
-
+#include <stdint.h>
 int strcmp(char * str1, char * str2);
 int strlen(char * string);
 int pow(int base,int n);
 int to_num(char * s , int dim);
-
+void numberToString(uint64_t number , char * string);
 #endif
